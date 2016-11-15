7c0f17139602c2ebfd6e251a63415cb283db0c0f
Diego Fonseca <diegofonseca@MacBook-Pro-de-Diego.local>
2016-11-15 13:38:48 -0200

    comentário adicionado

--- despensafacilx/ViewController.swift
+++ despensafacilx/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //início do projeto 15/11/16
     }
 
     override func didReceiveMemoryWarning() {
